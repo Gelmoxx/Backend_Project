2e48c484ff0149dd3ea310b1ab9f0185a467bf55
tperegrin <djtoso@gmail.com>
2022-02-08 15:29:25 +0100

    Vytvorenie triedy Kniha

--- src/Kniha.java
+++ src/Kniha.java
@@ -1,3 +1,24 @@
-public class Kniha {
+import java.util.Random;
 
+public class Kniha {
+    private String autor;
+    private String nazov;
+    protected int id;
+
+    public Kniha(String autor, String nazov) {
+        this.autor = autor;
+        this.nazov = nazov;
+    }
+
+    public void generujId(){
+        Random rd = new Random();
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getNazov() {
+        return nazov;
+    }
 }
